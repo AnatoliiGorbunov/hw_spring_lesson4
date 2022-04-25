77df570ddd7a5f1e6aeb961fa728310a406ee1d7
Aanatolii Gorbunov <617299@bk.ru>
2022-04-25 09:24:38 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/hw_spring_lesson4/HwSpringLesson4Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hw_spring_lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringLesson4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringLesson4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/hw_spring_lesson4/HwSpringLesson4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hw_spring_lesson4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringLesson4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
